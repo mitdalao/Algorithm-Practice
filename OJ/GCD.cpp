1246cf42fd7e25952e7eff88fde03cd045851d4c
shify <1215308692@qq.com>
2020-07-13 09:44:13 +0800

    Online Judge

---
+++ OJ/GCD.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int gcd(int a, int b){
+    while(b != 0){
+        int temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
+}
+int main(){
+    int a, b;
+    while(scanf("%d%d", &a, &b) != EOF){
+        printf("%d\n", gcd(a, b));
+    }
+    return 0;
+}
